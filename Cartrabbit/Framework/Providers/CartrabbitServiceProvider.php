278de7bd4ce45220ff7133e7472032b386285c9e
Ashlin <ashlin.flycart@gmail.com>
2017-03-22 11:46:10 +0530

    - set collation from wordpress

--- Cartrabbit/Framework/Providers/CartrabbitServiceProvider.php
+++ Cartrabbit/Framework/Providers/CartrabbitServiceProvider.php
@@ -129,8 +129,10 @@ class CartrabbitServiceProvider extends ServiceProvider {
             'database' => DB_NAME,
             'username' => DB_USER,
             'password' => DB_PASSWORD,
-            'charset' => DB_CHARSET,
-            'collation' => DB_COLLATE ?: $wpdb->collate,
+//            'charset' => DB_CHARSET,
+//            'collation' => DB_COLLATE ?: $wpdb->collate,
+            'charset' => $wpdb->charset,
+            'collation' => $wpdb->collate,
             'prefix' => $wpdb->prefix
         ]);
 
